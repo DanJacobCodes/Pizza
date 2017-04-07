fef2f30aee78f799ee2cb96b8f84fd14ccddf75b
Dan Jacob <dansamueljacob@gmail.com>
2017-04-07 10:07:13 -0700

    add initial project setup including bootstrap styled page

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//Business Logic//
+
+function Pizza(toppings, size){
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.price = function () {
+  if () {
+
+  }
+
+};
+
+
+
+//User Interface Logic//
+$(document).ready(function(){
+
+
+
+
+
+    
+}
